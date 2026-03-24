c81dc67582a6e9f64640a66845469d8c64e1a86f
janzgo789 <01197655@pw.edu.pl>
2026-03-24 21:40:05 +0100

    zaimplementowany algorytm tutte

--- tutte.h
+++ tutte.h
@@ -1,4 +1,13 @@
 #ifndef TUTTE_H
 #define TUTTE_H
 
+#include "graf.h"
+
+// Funkcja główna algorytmu Tutte'a
+// it - liczba iteracji do numerycznego wyznaczenia pozycji (metoda relaksacji)
+void oblicz_tutte(Graf *g, int it);
+
+// Funkcja pomocnicza do przypisania wierzchołkom zewnętrznym pozycji na okręgu
+void ustaw_zewnetrzne_na_kole(Graf *g, double promien);
+
 #endif //TUTTE_H
