c3da4b43fe9c5d15be71e3494437904c3bfc983d
janzgo789 <01197655@pw.edu.pl>
2026-03-25 20:14:20 +0100

    zaimplementowany algorytm tutte

--- tutte.h
+++ tutte.h
@@ -3,11 +3,13 @@
 
 #include "graf.h"
 
+// Funkcja wyszukująca zewnętrzny cykl (zgodna z dokumentacją)
+void wyznacz_zewnetrzny_cykl(Graf *g);
+
 // Funkcja główna algorytmu Tutte'a
-// it - liczba iteracji do numerycznego wyznaczenia pozycji (metoda relaksacji)
 void oblicz_tutte(Graf *g, int it);
 
-// Funkcja pomocnicza do przypisania wierzchołkom zewnętrznym pozycji na okręgu
+// Funkcja pomocnicza do rozmieszczenia wierzchołków na okręgu
 void ustaw_zewnetrzne_na_kole(Graf *g, double promien);
 
 #endif //TUTTE_H
